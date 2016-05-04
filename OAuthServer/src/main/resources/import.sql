bf65c82cc5e99f62713d3cbd99eb59c6697894ed
jsanc <joseluis.sanchez@ventura24.es>
2016-05-04 17:01:49 +0200

    Back to RC1. Now, works inside a docker-container Signed-off-by: jsanc <joseluis.sanchez@ventura24.es>

--- OAuthServer/src/main/resources/import.sql
+++ OAuthServer/src/main/resources/import.sql
@@ -1,2 +1,2 @@
-insert into users (username, password, enabled) values ('test','$2a$06$evSAxJgV5cdklr1CkEnFAeqUcyK/xhsPnEpLpKgjPlPglbMOf9X4m',true);
+insert into users (username, password, enabled) values ('test','$2a$06$evSAxJgV5cdklr1CkEnFAeqUcyK/xhsPnEpLpKgjPlPglbMOf9X4m','true');
 insert into user_roles (username, role) values ('test','role_user');
